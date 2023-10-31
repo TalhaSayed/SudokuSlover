1765711af328594b59a12aad8addb382cad5d245
TalhaSayed <143438967+TalhaSayed@users.noreply.github.com>
2023-10-31 19:26:44 +0530

    Add files via upload

---
+++ SudokuDigit.java
@@ -0,0 +1,105 @@
+package Game.Sudoku;
+import java.util.ArrayList;
+
+/* Class representing a sudoku digit
+ *
+ * Author: Matt Tacchino
+ *
+ * A sudoku digit is a digit from 1 to 9,
+ * or an array of possible values. A digit can
+ * can also be "safe" if it was an original input.
+ * Original inputs should never be changed after they
+ * are set
+ *
+ */
+public class SudokuDigit {
+    private int answer;
+    private int xLocation;
+    private int yLocation;
+    private ArrayList<Integer> possibleValues = new ArrayList<Integer>();
+    private boolean isSafe = false;
+
+    //Constructor for when answer is known
+    SudokuDigit(int answer){
+        this.answer = answer;
+    }
+
+    //Constructor for unknown digit. Set possible values to be all digits from 1 to 9
+    SudokuDigit(int xLocation, int yLocation){
+        for (int i = 0; i < 9; i++){
+            possibleValues.add(i+1);
+        }
+        this.xLocation = xLocation;
+        this.yLocation = yLocation;
+    }
+
+    //copy constructor
+    SudokuDigit(SudokuDigit sudokuDigit){;
+        this.answer = sudokuDigit.answer;
+        this.xLocation = sudokuDigit.xLocation;
+        this.yLocation = sudokuDigit.yLocation;
+        this.possibleValues = sudokuDigit.possibleValues;
+        this.isSafe = sudokuDigit.isSafe;
+    }
+
+
+    //getters
+    public ArrayList<Integer> getPossibleValues(){
+        return possibleValues;
+    }
+
+    public boolean isSolved(){
+        return (answer != 0);
+    }
+
+    public boolean isSafe(){
+        return isSafe;
+    }
+
+    public int getAnswer(){
+        return answer;
+    }
+
+    public int getX(){
+        return xLocation;
+    }
+
+    public int getY(){
+        return yLocation;
+    }
+
+    //setters
+    public void setSafe(boolean isSafe){
+        this.isSafe = isSafe;
+    }
+
+    public void setAnswer(int answer){
+        this.answer = answer;
+        possibleValues.clear();
+        if (answer == 0) { // add all possible values from 1 to 9
+            for (int i = 1; i < 10; i ++)
+                this.possibleValues.add(i);
+        }
+    }
+
+    public void removePossibleValue(int possibleValue){
+        if (answer == 0) {
+            if (possibleValues.contains(possibleValue))
+                possibleValues.remove(possibleValue);
+            if (possibleValues.size() == 1)
+                setAnswer(possibleValues.get(0));
+        }
+    }
+
+    public String toString(){
+        if (answer == 0){
+            String str = "Possible values for location (" + xLocation + "," + yLocation + "): ";
+            for (int value : possibleValues)
+                str = str + value + " ";
+            return str;
+        }
+        else
+            return "Answer for location (" + xLocation + "," + yLocation + "): " + answer;
+    }
+}
+
---
+++ SudokuStart.java
@@ -0,0 +1,19 @@
+package Game.Sudoku;
+
+/*  Class containing the main method to create a GUI
+ *
+ *  Author: Matt Tacchino
+ *
+ *  This program creates a GUI to input values in a sudoku table,
+ *  then solve the puzzle using recursive backtracking.
+ *  Also includes sample puzzles for testing
+ *
+ *
+ */
+public class SudokuStart {
+    public static void main (String args[]){
+        GUI gui = new GUI();
+        gui.createGUI();
+    }
+}
+
